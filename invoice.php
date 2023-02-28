7e07b4ed721d6f2b1c21d703a6a946e074d44204
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-02-28 16:00:38 +0700

    update

--- invoice.php
+++ invoice.php
@@ -26,16 +26,42 @@
                                 <tbody>
                                     <?php
                                     require 'database_connection.php';
-                                    $display_query = "SELECT T1.id, T1.rec_fullname, T1.edo_name, T1.dateCreate FROM receipt T1";
+
+                                    $display_query = "SELECT T1.id, T1.rec_fullname, T1.edo_name, T1.rec_date FROM receipt T1";
                                     $results = mysqli_query($con, $display_query);
                                     $count = mysqli_num_rows($results);
+
                                     if ($count > 0) {
+                                        // Format date in Thai language
+                                        $thai_months = array(
+                                            "01" => "มกราคม",
+                                            "02" => "กุมภาพันธ์",
+                                            "03" => "มีนาคม",
+                                            "04" => "เมษายน",
+                                            "05" => "พฤษภาคม",
+                                            "06" => "มิถุนายน",
+                                            "07" => "กรกฎาคม",
+                                            "08" => "สิงหาคม",
+                                            "09" => "กันยายน",
+                                            "10" => "ตุลาคม",
+                                            "11" => "พฤศจิกายน",
+                                            "12" => "ธันวาคม"
+                                        );
+
                                         while ($data_row = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
+                                            // Retrieve date value from database
+                                            $date = $data_row['rec_date'];
+
+                                            // Format date in Thai language
+                                            $year = date('Y', strtotime($date)) + 543;
+                                            $month = date('m', strtotime($date));
+                                            $month_thai = $thai_months[$month];
+                                            $day = date('d', strtotime($date));
                                     ?>
                                             <tr>
                                                 <td><?php echo $data_row['rec_fullname']; ?></td>
                                                 <td><?php echo $data_row['edo_name']; ?></td>
-                                                <td><?php echo $data_row['dateCreate']; ?></td>
+                                                <td><?php echo $day . ' ' . $month_thai . ' ' . $year; ?></td>
                                                 <td>
                                                     <a href="pdf_maker.php?id=<?php echo $data_row['id']; ?>&ACTION=VIEW" target="_blank" class="btn btn-success"><i class="fa fa-file-pdf-o"></i> View PDF</a>
                                                 </td>
@@ -47,6 +73,7 @@
                                         }
                                     }
                                     ?>
+
                                 </tbody>
                             </table>
                         </form>
--- pdf_maker.php
+++ pdf_maker.php
@@ -4,11 +4,12 @@ include_once('TCPDF/tcpdf.php');
 
 $id = $_GET['id'];
 
-$inv_mst_query = "SELECT T1.id, T1.edo_name, T1.edo_pro_id, T1.rec_fullname,T1.rec_money,T1.address FROM receipt T1 WHERE T1.id='" . $id . "' ";
+$inv_mst_query = "SELECT T1.id, T1.edo_name, T1.edo_pro_id,T1.rec_date, T1.rec_fullname,T1.rec_money,T1.address FROM receipt T1 WHERE T1.id='" . $id . "' ";
 $inv_mst_results = mysqli_query($con, $inv_mst_query);
 $count = mysqli_num_rows($inv_mst_results);
 if ($count > 0) {
 	$inv_mst_data_row = mysqli_fetch_array($inv_mst_results, MYSQLI_ASSOC);
+	
 
 	//----- Code for generate pdf
 	$pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
@@ -23,53 +24,15 @@ if ($count > 0) {
 	$pdf->setPrintHeader(false);
 	$pdf->setPrintFooter(false);
 	$pdf->SetAutoPageBreak(TRUE, 10);
-	$pdf->SetFont('thsarabunnew', '', 11.8);
+	$pdf->SetFont('thsarabunnew', '', 14);
 	$pdf->SetMargins(10, 3, 10);
 	$pdf->AddPage(); //default A4
 	// 
 	date_default_timezone_set('Asia/Bangkok');
 	$year = date('Y') + 543;
 	$datetime_be = str_replace(date('Y'), $year, date('Y'));
-	$datetime_be1 = str_replace(date('Y'), $year, date('Y-m-d'));
 	// 
 
-	// 
-	// Define an array with Thai words for numbers 0-9
-	$thai_num_arr = array(
-		'๐', '๑', '๒', '๓', '๔', '๕', '๖', '๗', '๘', '๙'
-	);
-
-	// Define an array with Thai words for powers of 10
-	$thai_unit_arr = array(
-		'', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน', 'ล้าน'
-	);
-
-	// Define the number you want to convert to Thai words
-	$number = $inv_mst_data_row['rec_money'];
-
-	// Convert the number to an array of digits
-	$digits = str_split(str_replace(',', '', $number));
-
-	// Reverse the array of digits so we can loop through it from right to left
-	$digits = array_reverse($digits);
-
-	// Initialize the Thai words string
-	$thai_words = '';
-
-	// Loop through the array of digits
-	for ($i = 0; $i < count($digits); $i++) {
-		// If the current digit is not 0, add its Thai word to the Thai words string
-		if ($digits[$i] != 0) {
-			$thai_words .= $thai_num_arr[$digits[$i]];
-			$thai_words .= $thai_unit_arr[$i % 6];
-		}
-	}
-
-	// Reverse the Thai words string to get the correct order
-	$thai_words = strrev($thai_words);
-
-	// Print the Thai words
-	echo $thai_words;
 
 	// 
 	$content = '';
@@ -89,40 +52,124 @@ if ($count > 0) {
     <td colspan="2"></td>
   </tr>
 </table>
-	<tr><td><b>มหาวิทยาลัยเชียงใหม่</b></td>
-	<td align="right"><b>ใบเสร็จรับเงิน</b></td></tr>
-	<tr><td><b>Chiang Mai University</b></td>
-	<td align="right"><b>ต้นฉบับ</b></td></tr>
-	<tr><td>239 ถนนห้วยแก้ว ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200</td>
-	<td align="right">คณะพยาบาลศาสตร์ มหาวิทยาลัยเชียงใหม่</td></tr>
-	<tr><td>239 Huaykaew Road, Muang District, Chiang Mai, 50200</td>
-	<td align="right">Faculty of Nursing, CMU</td></tr>
-	<tr><td>เบอร์โทร 053-949075</td>
-	<td align="right">110/406 ถนนอินทวโรรส ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200</td></tr>
-	<tr><td>เลขประจำตัวผู้เสียภาษีอากร/Taxpayer identification Number 099 4 00042317 9</td>
-	<td align="right">110/406 Inthawaroros Road, Suthep, Chiang Mai 50200</td></tr>
-	<tr><td></td>
-	<td align="right">เบอรโทร 053-949075</td></tr>
-	
 
-	<tr><td><b>ชื่อ : </b>' . $inv_mst_data_row['rec_fullname'] . ' </td>
-	<td align="right"><b>เลขที่ใบเสร็จ: </b>' . $datetime_be . '-' . $inv_mst_data_row['edo_pro_id'] . '-' . $inv_mst_data_row['id'] . '</td></tr>
-	<tr><td><b>ที่อยู่ : </b>' . $inv_mst_data_row['address'] . ' </td>
-	<td align="right"><b>วันที่เอกสาร : </b>' . $datetime_be1 . '</td></tr>
-	<tr><td><b>รายละเอียดโครงการ</b><br>' . $inv_mst_data_row['edo_name'] . ' </td>
-	<td align="right"><b>จำนวนเงิน</b><br>' . $inv_mst_data_row['rec_money'] . ' บาท</td></tr>
-	<tr><td align="right"><b>จำนวนเงินรวม : </b>' . $inv_mst_data_row['rec_money'] . ' บาท</td></tr>
-	<tr><td><b>รวมทั้งหมด : </b>' . $inv_mst_data_row['rec_money'] . ' </td></tr>
-	<tr><td><b>ชำระจำนวนเงิน : </b>' . $inv_mst_data_row['rec_money'] . ' </td></tr>
-	<tr><td><b>ชำระด้วย : </b>' . $inv_mst_data_row[''] . ' </td></tr>
+<tr>
+	<td>
+		<b>มหาวิทยาลัยเชียงใหม่</b>
+	</td>
+	<td align="right">
+		<b>ใบเสร็จรับเงิน</b></td>
+</tr>
 
-	<tr><td>' . $inv_mst_data_row[''] . ' </td>
-	<td align="right">(นางสาวชนิดา ต้นพิพัฒน์)<br>เจ้าหน้าที่ผู้รับเงิน<br>วันที่ : ' . $datetime_be1 . '</td></tr>
-	<table><tr><td><b>หมายเหตู :ใบเสร็จรับเงินจะมีผลสมบูรณ์ต่อเมื่อได้รับชำระเงินเรียบร้อยแล้วและมีลายเซ็นของผู้รับเงินครบถ้วน</b></td></tr>
+<tr>
+	<td>
+		<b>Chiang Mai University</b>
+	</td>
+	<td align="right">
+		<b>ต้นฉบับ</b>
+	</td>
+</tr>
+
+<tr>
+	<td>239 ถนนห้วยแก้ว ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200</td>
+	<td align="right">คณะพยาบาลศาสตร์ มหาวิทยาลัยเชียงใหม่</td>
+</tr>
+
+<tr>
+	<td>239 Huaykaew Road, Muang District, Chiang Mai, 50200</td>
+	<td align="right">Faculty of Nursing, CMU</td>
+</tr>
+
+<tr>
+	<td>เบอร์โทร 053-949075</td>
+	<td align="right">110/406 ถนนอินทวโรรส ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200</td>
+</tr>
+
+<tr>
+	<td>เลขประจำตัวผู้เสียภาษีอากร/Taxpayer identification Number </td>
+	<td align="right">110/406 Inthawaroros Road, Suthep, Chiang Mai 50200</td>
+</tr>
+
+<tr>
+	<td>099 4 00042317 9</td>
+	<td align="right">เบอร์โทร 053-949075</td>
+</tr>
+
+<br>
+<tr>
+	<td><b>ชื่อ : </b>' . $inv_mst_data_row['rec_fullname'] . ' </td>
+	<td align="right"><b>เลขที่ใบเสร็จ: </b>' . $datetime_be . '-' . $inv_mst_data_row['edo_pro_id'] . '-' . $inv_mst_data_row['id'] . '</td>
+</tr>
+
+<tr>
+	<td><b>ที่อยู่ : </b>' . $inv_mst_data_row['address'] . ' </td>
+	<td align="right"><b>วันที่เอกสาร : </b>' . $inv_mst_data_row['rec_date'] . '</td>
+</tr>
+
+<tr>
+	<td><b>รายละเอียดโครงการ</b><br>' . $inv_mst_data_row['edo_name'] . ' </td>
+	<td align="right"><b>จำนวนเงิน</b><br>' . $inv_mst_data_row['rec_money'] . ' บาท</td>
+</tr>
+
+<tr>
+	<td align="right"><b>จำนวนเงินรวม : </b>' . $inv_mst_data_row['rec_money'] . ' บาท</td>
+</tr>
+
+<tr>
+	<td><b>รวมทั้งหมด : </b>' . $inv_mst_data_row['rec_money'] . ' </td>
+</tr>
+
+<tr>
+	<td><b>ชำระจำนวนเงิน : </b>' . $inv_mst_data_row['rec_money'] . ' </td>
+</tr>
 	<tr>
-    <td style="border-bottom: 0.5px solid black;"></td>
-  </tr>
-	</table>
+<td>
+	<b>ชำระด้วย : </b>' . $inv_mst_data_row[''] . ' </td>
+</tr>
+
+<tr>
+	<td>' . $inv_mst_data_row[''] . ' </td>
+	<td align="right">(นางสาวชนิดา ต้นพิพัฒน์)<br>เจ้าหน้าที่ผู้รับเงิน<br>วันที่ : ' . $datetime_be1 . '</td>
+</tr>
+
+<table>
+	<tr>
+		<td><b>หมายเหตู :ใบเสร็จรับเงินจะมีผลสมบูรณ์ต่อเมื่อได้รับชำระเงินเรียบร้อยแล้วและมีลายเซ็นของผู้รับเงินครบถ้วน</b></td>
+	</tr>
+</table>
+	<tr>
+		<td style="border-bottom: solid black, 1px;"></td>
+	</tr>
+	<br>
+	<tr>
+		<td style="text-align: center;"><b>อนุโมทนาบัตร</b></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;"><b>คณะพยาบาลศาสตร์ มหาวิทยาลัยเชียงใหม่</b></td>
+	</tr>
+	<tr>
+		<td style="text-align: center;">ได้รับเงินบริจาคเป็นจำนวนเงิน ' . $inv_mst_data_row['rec_money'] . ' บาท ' . $inv_mst_data_row['rec_money'] . '</td>
+	</tr>
+	<br>
+	<tr>
+		<td><b>จาก : </b>' . $inv_mst_data_row['rec_fullname'] . ' </td>
+	</tr>
+	<tr>
+		<td><b>วัตถุประสงค์ : </b><br>' . $inv_mst_data_row['edo_name'] . ' </td>
+	</tr>
+	<br>
+	<tr>
+		<td style="text-align: center;">ขอให้กุศลผลบุญจากการบริจาคของท่านในครั้งนี<br>โปรดดลบันดาลให้ท่านประสบแต่ความสุขสวัสดี ปราศจากทุกข์โศกโรคภัย<br>ปราถนาสิ่งใดให้สำเร็จสมดังประสงค์ทุกประการ<br>ให้ไว้ ณ วันที่  ' . $inv_mst_data_row['rec_date'] . '</td>
+	</tr>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<tr>
+		<td style="text-align: center;"><b>(ผู้ช่วยศาสตราจารย์ ดร.ธานี แก้วธรรมานุกูล)<br>คณบดีคณะพยาบาลศาสตร์</b></td>
+	</tr>
+	
 	';
 
 	$pdf->writeHTML($content);
