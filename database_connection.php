dbda95399d853226915f99dcc57990f06c3331b8
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-09-18 09:31:16 +0700

    updata

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
 $hostname = "localhost";
-$username = "root";
-$password = "";
+$username = "edonation";
+$password = "edonate@FON";
 $database = "edonation";
 $con = mysqli_connect($hostname, $username, $password, $database);
  
